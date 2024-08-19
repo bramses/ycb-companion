815666862ebbc7953ec77d8635f69fd2f5c78880
Bram Adams <3282661+bramses@users.noreply.github.com>
2024-08-19 01:54:31 -0400

    feat: Add "Create Upload w/ ChatGPT" link to upload page

--- src/app/[locale]/(auth)/dashboard/upload/page.tsx
+++ src/app/[locale]/(auth)/dashboard/upload/page.tsx
@@ -252,6 +252,28 @@ const Upload = () => {
       >
         Clear Cache
       </button>
+      <a
+        href="https://chatgpt.com/g/g-L05Oy6FrY-create-ycb-entry"
+        target="_blank"
+        className="inline-flex items-center font-medium text-blue-600 hover:underline"
+      >
+        Create Upload w/ ChatGPT
+        <svg
+          className="ms-2.5 size-3 rtl:rotate-[270deg]"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 18 18"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+          />
+        </svg>
+      </a>
     </div>
   );
 };
