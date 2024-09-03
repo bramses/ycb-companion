628add34fde41ea5251757ee3f6e6518873a4325
Bram Adams <3282661+bramses@users.noreply.github.com>
2024-09-03 10:15:24 -0400

    feat: Update viewport meta tag for better responsiveness
    The code changes in this commit update the viewport meta tag in the `layout.tsx` file to improve responsiveness. The `content` attribute is modified to include `user-scalable=0`, which prevents users from zooming in or out on the page. This change ensures that the page is displayed properly on different devices with varying screen sizes.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout(props: {
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
       </head>
       <body>
