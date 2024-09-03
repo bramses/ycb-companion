6be9bf939843fe644ff0ab10c5128e041c81df48
Bram Adams <3282661+bramses@users.noreply.github.com>
2024-09-03 01:52:55 -0400

    Optimize SearchBox and Entry components with lazy loading for images

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -3,15 +3,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import { fetchSearchEntries } from '../helpers/functions';
 
 const SearchResults = () => {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const [textAreaValue, setTextAreaValue] = useState('');
   const [showLoading, setShowLoading] = useState(false);
   const [searchResults, setSearchResults] = useState<any[]>([]);
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
+  const [rndmBtnText, setRndmBtnText] = useState('Random');
 
   const createQueryString = useCallback(
     (name: string, value: string) => {
@@ -30,126 +34,52 @@ const SearchResults = () => {
     }
   }, [searchParams]);
 
-  async function fetchFavicon(url: string) {
-    const response = await fetch(`/api/favicon?url=${url}`);
-    const data = await response.json();
-    return data;
-  }
-
-  const fetchByID = async (entryId: string) => {
-    const response = await fetch('/api/fetch', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: entryId,
-      }),
-    });
-
-    const resData = await response.json();
-
-    const entry = resData.data;
-    // parse metadata
-    const metadata = JSON.parse(entry.metadata);
-
-    return {
-      data: entry.data,
-      metadata,
-    };
-  };
-
-  async function fetchSearchEntries() {
-    const response = await fetch('/api/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query: textAreaValue }),
-    });
-    const data = await response.json();
-    const entries = data.data;
-    // map entries.metadata to json
-    const parsedEntries = entries.map((entry: any) => {
-      let metadata;
-      try {
-        metadata = JSON.parse(entry.metadata);
-      } catch (err) {
-        metadata = entry.metadata; // fallback to original metadata if parsing fails
-      }
-
-      return { ...entry, metadata, favicon: '/favicon.ico' };
-    });
-
-    // Return parsed entries immediately
-    setSearchResults(parsedEntries);
-
-    // for all entries with a parent_id, fetch parent_id data and append data as parentData key
-    const parentPromises = parsedEntries.map((entry: any) => {
-      if ('parent_id' in entry.metadata) {
-        return fetchByID(entry.metadata.parent_id);
-      }
-      return Promise.resolve(null); // Return null for entries without parent_id
-    });
-
-    Promise.allSettled(parentPromises).then((results) => {
-      const updatedEntries = parsedEntries.map((entry: any, index: any) => {
-        if (!results[index]) {
-          return entry;
-        }
-        if (results[index].status === 'fulfilled' && results[index].value) {
-          return { ...entry, parentData: results[index].value };
-        }
-        return entry;
-      });
-
-      console.log('updatedEntries:', updatedEntries);
-
-      // Return updated entries immediately
-      setSearchResults(updatedEntries);
-
-      // fetch favicon for each entry
-      const faviconPromises = updatedEntries.map((entry: any) => {
-        if (!entry.metadata) {
-          return { favicon: '/favicon.ico' };
-        }
-
-        return fetchFavicon(entry.metadata.author);
-      });
-
-      Promise.all(faviconPromises).then((favicons) => {
-        const updatedEntriesFavicon = updatedEntries.map(
-          (entry: any, index: any) => {
-            const favicon = favicons[index].favicon
-              ? favicons[index].favicon
-              : '/favicon.ico';
-            return { ...entry, favicon };
-          },
-        );
-
-        // Update search results with favicons
-        setSearchResults(updatedEntriesFavicon);
-      });
-    });
-
-    return parsedEntries;
-  }
-
   const handleSearch = async () => {
     router.push(`${pathname}?${createQueryString('query', textAreaValue)}`);
 
     setShowLoading(true);
 
-    const parsedEntries = await fetchSearchEntries();
+    const parsedEntries = await fetchSearchEntries(
+      textAreaValue,
+      setSearchResults,
+    );
 
     setSearchResults(parsedEntries);
 
     setShowLoading(false);
   };
 
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if ((e.metaKey || e.ctrlKey) && e.key === 'k') {
+        if (textAreaRef.current) {
+          e.preventDefault();
+          textAreaRef.current.focus();
+          // highlight the text in the textarea
+          textAreaRef.current.select();
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  // if routed to with no params focus the textarea
+  useEffect(() => {
+    if (searchParams.toString() === '') {
+      if (textAreaRef.current) {
+        textAreaRef.current.focus();
+      }
+    }
+  }, [searchParams]);
+
   return (
     <div>
       <textarea
+        ref={textAreaRef}
         id="message"
         rows={2}
         className="sticky top-2 z-50 mt-4 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
@@ -171,6 +101,42 @@ const SearchResults = () => {
       >
         Search
       </button>
+      <div className="mt-4 flex space-x-2">
+        {/* a btn to take the text in textarea and search google with it in a new tab */}
+        <button
+          type="button"
+          onClick={() => {
+            window.open(
+              `https://www.google.com/search?q=${textAreaValue}`,
+              '_blank',
+            );
+          }}
+          className="mb-2 me-2 w-full rounded-lg border border-gray-300 px-5 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300"
+        >
+          Search the Web
+        </button>
+
+        {/* a btn to call /api/random and put the text in the textarea */}
+        <button
+          type="button"
+          onClick={async () => {
+            setRndmBtnText('Loading...');
+            const response = await fetch('/api/random', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+            const data = await response.json();
+            // console.log('Random data:', data);
+            setTextAreaValue(data.data.data);
+            setRndmBtnText('Random');
+          }}
+          className="mb-2 me-2 w-full rounded-lg border border-gray-300 px-5 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300"
+        >
+          {rndmBtnText}
+        </button>
+      </div>
       {showLoading && <div>Loading...</div>}
       {searchResults.map((result) => (
         <div key={result.id} className="mb-4">
