4ebf0fa8dd7a158fffa3dea337d50d58ca5f773a
Bram Adams <3282661+bramses@users.noreply.github.com>
2024-09-05 15:31:31 -0400

    Optimize rendering of author URLs in EntryPage and SearchResults components

--- src/app/[locale]/(auth)/dashboard/garden/page.tsx
+++ src/app/[locale]/(auth)/dashboard/garden/page.tsx
@@ -2,70 +2,30 @@
 
 import 'react-calendar/dist/Calendar.css';
 
-import { useRouter, useSearchParams } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import ScrollToTop from 'react-scroll-to-top';
 
-import Entry from '@/components/Entry';
-import { getCache, invalidateCache, setCache } from '@/helpers/old_cache';
+import { splitIntoWords } from '@/helpers/functions';
 
 const GardenDaily = () => {
-  const router = useRouter();
   const searchParams = useSearchParams();
   const [entries, setEntries] = useState<any[]>([]);
   const [selectedDay, setSelectedDay] = useState('');
   const [dateSelected, setDateSelected] = useState(new Date());
   const [loading, setLoading] = useState(false);
-  const cache = getCache();
+  // const cache = getCache();
 
-  const fetchByID = useCallback(
-    async (id: string) => {
-      const cachedAlias = cache.aliases[id];
-      if (cachedAlias) {
-        // console.log('Returning cached alias:', cachedAlias);
-        return { data: cachedAlias };
-      }
-
-      // TODO: this breaks w aliases updating and idk how to fix atm
-      // const cachedParent = cache.parents[id];
-      // if (cachedParent) {
-      //   // console.log('Returning cached parent:', cachedParent);
-      //   return { data: cachedParent };
-      // }
-
-      try {
-        const response = await fetch('/api/fetch', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            id,
-          }),
-        });
-        const data = await response.json();
-        const entry = data.data;
-        // parse metadata
-        const metadata = JSON.parse(entry.metadata);
-
-        // cache the entry
-        if (metadata.parent_id) {
-          cache.aliases[id] = entry;
-        } else {
-          cache.parents[id] = entry;
-        }
-
-        setCache(cache);
-
-        return data;
-      } catch (error) {
-        console.error('Error fetching entry by ID:', error);
-        return {};
-      }
-    },
-    [cache],
-  );
+  const toHostname = (url: string) => {
+    try {
+      return new URL(url).hostname;
+    } catch (err) {
+      return url;
+    }
+  };
 
   const fetchRecords = useCallback(
     async (date: Date) => {
@@ -95,64 +55,14 @@ const GardenDaily = () => {
         body: JSON.stringify({ date: dateString }),
       });
       const responseData = await response.json();
-      // console.log('Fetched records:', responseData);
-      // map over the response data and turn metadata into an object
+
+      console.log('Fetched records:', responseData);
       // set entries to the mapped data
-
-      const mappedData = responseData.data.map((entry: any) => {
-        const metadata = JSON.parse(entry.metadata);
-        let links = [];
-
-        if (metadata.links) {
-          links = metadata.links;
-        }
-
-        return {
-          ...entry,
-          metadata: JSON.parse(entry.metadata),
-          links,
-        };
-      });
-
-      // get existing aliases for each entry
-      const aliasData = await Promise.all(
-        mappedData.map(async (entry: any) => {
-          if (entry.metadata.alias_ids) {
-            try {
-              const aliasDataMap = await Promise.all(
-                entry.metadata.alias_ids.map(async (aliasId: string) => {
-                  try {
-                    const aliasEntryRes = await fetchByID(aliasId);
-                    const aliasEntry = aliasEntryRes.data;
-                    return { data: aliasEntry.data, id: aliasEntry.id };
-                  } catch (aliasFetchError) {
-                    console.error(
-                      `Error fetching alias entry with ID ${aliasId}:`,
-                      aliasFetchError,
-                    );
-                    throw aliasFetchError;
-                  }
-                }),
-              );
-
-              return {
-                ...entry,
-                aliasData: aliasDataMap,
-              };
-            } catch (aliasError) {
-              console.error('Error fetching aliases:', aliasError);
-              return { ...entry };
-            }
-          }
-          return entry;
-        }),
-      );
-
-      setLoading(false);
+      setEntries(responseData.data);
       setDateSelected(date);
-      setEntries(aliasData);
+      setLoading(false);
     },
-    [fetchByID, setSelectedDay, setLoading, setEntries],
+    [setSelectedDay, setLoading, setEntries],
   );
 
   // on load page, fetch all entries for the current day
@@ -183,139 +93,6 @@ const GardenDaily = () => {
     fetchRecords(dateParam);
   }, [searchParams]);
 
-  const addEntry = async (data: string, metadata: string) => {
-    try {
-      const response = await fetch('/api/add', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          data,
-          metadata,
-        }),
-      });
-      const responseData = await response.json();
-
-      // console.log('Added entry:', responseData);
-      return responseData;
-    } catch (error) {
-      console.error('Error adding entry:', error);
-      return {};
-    }
-  };
-
-  const updateEntry = async (
-    id: string,
-    data: string,
-    metadata: string,
-    isAlias: boolean,
-  ) => {
-    try {
-      invalidateCache(id, false);
-      const response = await fetch('/api/update', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id,
-          data,
-          metadata,
-        }),
-      });
-      const responseData = await response.json();
-
-      if (isAlias) {
-        invalidateCache(id, isAlias);
-        cache.aliases[id] = responseData.data;
-      } else {
-        invalidateCache(id, isAlias);
-        cache.parents[id] = responseData.data;
-      }
-
-      return responseData;
-    } catch (error) {
-      console.error('Error updating entry:', error);
-      return {};
-    }
-  };
-
-  const addLink = async (id: string, url: string, name: string) => {
-    // update metadata with new link added to metadata.links
-    // fetch the entry by id
-    // update the entry with the new metadata
-    // add the link to the entry's data
-
-    try {
-      const entryRes = await fetchByID(id);
-      const entry = entryRes.data;
-      const { data } = entry;
-      let metadataJSON = entry.metadata;
-      try {
-        metadataJSON = JSON.parse(entry.metadata);
-      } catch (err) {
-        console.error('Error parsing metadata:', err);
-      }
-
-      // add the link to the metadata
-      metadataJSON.links = metadataJSON.links
-        ? [...metadataJSON.links, { url, name }]
-        : [{ url, name }];
-
-      console.log('metadataJSON:', metadataJSON);
-      console.log('metadataJSON.links:', metadataJSON.links);
-      console.log('data:', data);
-      console.log('id:', id);
-
-      // update the entry with the new metadata
-      await updateEntry(id, data, metadataJSON, false);
-
-      return metadataJSON;
-    } catch (err) {
-      return { error: err };
-    }
-  };
-
-  // TODO: extract these into helper logic for better reusability bw here and SearchBox component
-  const handleAliasAdd = async (data: any) => {
-    // get id of the selected alias
-    // fetch the entry by id
-    invalidateCache(data.id, false);
-    const parentEntry = await fetchByID(data.id);
-    let parentAliases = [];
-    try {
-      parentAliases = JSON.parse(parentEntry.data.metadata).alias_ids;
-      // add a new entry with the alias as data and the original entry's metadata
-      // add parent_id to the metadata
-      const parentId = data.id;
-      const newMetadata = {
-        ...data.metadata,
-        parent_id: parentId,
-      };
-      const aliasRes = await addEntry(data.alias, newMetadata);
-      const aliasId = aliasRes.respData.id;
-      // update the original entry's metadata with the new alias id in the alias_ids array
-      const updatedMetadata = {
-        ...data.metadata,
-        alias_ids: parentAliases ? [parentAliases, aliasId].flat() : [aliasId],
-      };
-      await updateEntry(parentId, data.data, updatedMetadata, false);
-      invalidateCache(data.id, false);
-      return aliasRes;
-    } catch (err) {
-      console.error('Error parsing parent metadata:', err);
-      return { error: 'Error parsing parent metadata' };
-    }
-  };
-
-  const onDelve = async (data: string) => {
-    // console.log('Delving into:', data);
-    const search = new URLSearchParams();
-    search.set('q', data);
-    router.push(`/dashboard?${search.toString()}`);
-  };
-
   return (
     <div>
       {/* center the calendar */}
@@ -332,62 +109,80 @@ const GardenDaily = () => {
         </small>
       </h1>
       {loading && <p>Loading...</p>}
-      {entries.length === 0 ? (
-        <p>No entries for this day</p>
-      ) : (
-        entries.map((entry: any) => (
-          <Entry
-            key={'aliasData' in entry ? Math.random() * 1000 : entry.id} // Replace 'Math.random() * 1000' with a more specific key if possible
-            data={entry.data}
-            id={entry.id}
-            aliases={'aliasData' in entry ? entry.aliasData : []}
-            selectedIndex={'selectedIndex' in entry ? entry.selectedIndex : -1}
-            title={entry.metadata.title}
-            author={entry.metadata.author}
-            createdAt={entry.createdAt}
-            similarity={entry.similarity}
-            hasYouTubeEmbed={entry.metadata.author.includes('youtube.com')}
-            youtubeId={
-              entry.metadata.author.includes('youtube')
-                ? entry.metadata.author.split('v=')[1]?.split('&')[0]
-                : ''
-            }
-            displayDelve={false}
-            displayMetadata={false}
-            youtubeStart={
-              entry.metadata.author.includes('youtube') &&
-              entry.metadata.author.includes('t=')
-                ? entry.metadata.author.split('t=')[1].split('s')[0]
-                : ''
-            }
-            hasImage={entry.metadata.author.includes('imagedelivery.net')}
-            imageUrl={
-              entry.metadata.author.includes('imagedelivery.net')
-                ? entry.metadata.author
-                : ''
-            }
-            hasTwitterEmbed={
-              entry.metadata.author.includes('twitter.com') ||
-              /^https:\/\/(www\.)?x\.com/.test(entry.metadata.author)
-            }
-            tweetId={
-              entry.metadata.author.includes('twitter.com') ||
-              /^https:\/\/(www\.)?x\.com/.test(entry.metadata.author)
-                ? entry.metadata.author.split('/').pop()
-                : ''
-            }
-            hasInstagramEmbed={entry.metadata.author.includes('instagram.com')}
-            hasTikTokEmbed={entry.metadata.author.includes('tiktok.com')}
-            onAddAlias={handleAliasAdd}
-            hasAliases={'aliasData' in entry}
-            onDelve={onDelve}
-            onEdit={updateEntry}
-            hasLinks={'links' in entry}
-            links={'links' in entry ? entry.links : []}
-            onAddLink={addLink}
-          />
-        ))
-      )}
+      {entries.length === 0 ? <p>No entries for this day</p> : null}
+
+      {entries.map((result) => (
+        <div key={result.id} className="mb-4">
+          <Link
+            href={{
+              pathname: `/dashboard/entry/${result.id}`,
+            }}
+            className="block"
+          >
+            <div className="flex items-center text-blue-600 hover:underline">
+              <Image
+                src={result.favicon || '/favicon.ico'}
+                alt="favicon"
+                width={16}
+                height={16}
+                className="mr-2"
+              />
+              <span className="font-medium">
+                {result.data.split(' ').length > 12 ? (
+                  <>
+                    {splitIntoWords(result.data, 12, 0)}...
+                    <span className="mt-1 block text-sm text-gray-500">
+                      ...{splitIntoWords(result.data, 20, 12)}...
+                    </span>
+                  </>
+                ) : (
+                  result.data
+                )}
+              </span>
+            </div>
+            <div className="text-sm text-gray-500">
+              {result.parentData && (
+                <span className="mt-1 block">{result.parentData.data}</span>
+              )}
+            </div>
+          </Link>
+          {/* when was the entry created and updated */}
+          <div className="text-sm text-gray-500">
+            Created: {new Date(result.createdAt).toLocaleString()}
+            {result.createdAt !== result.updatedAt && (
+              <>
+                {' '}
+                | Last Updated: {new Date(
+                  result.updatedAt,
+                ).toLocaleString()}{' '}
+              </>
+            )}
+          </div>
+          <a
+            target="_blank"
+            href={result.metadata.author}
+            rel="noopener noreferrer"
+            className="inline-flex items-center font-medium text-blue-600 hover:underline"
+          >
+            {toHostname(result.metadata.author)}
+            <svg
+              className="ms-2.5 size-3 rtl:rotate-[270deg]"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 18"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+              />
+            </svg>
+          </a>
+        </div>
+      ))}
       <ScrollToTop smooth />
     </div>
   );
