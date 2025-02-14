ca1c96b6a7792bfa51b85319c37ba7312fcad1bb
Bram Adams <3282661+bramses@users.noreply.github.com>
2025-02-14 17:33:15 -0500

    refactor oidc.ts to require non-nullable env vars for configuration, removing console log for OIDC_AUTHORITY

--- src/libs/oidc.ts
+++ src/libs/oidc.ts
@@ -1,12 +1,10 @@
 import { UserManager } from 'oidc-client-ts';
 
-console.log('process.env.OIDC_AUTHORITY:', process.env.OIDC_AUTHORITY);
-
 const oidcConfig = {
-  authority: process.env.OIDC_AUTHORITY,
-  client_id: process.env.OIDC_CLIENT_ID,
-  redirect_uri: process.env.OIDC_REDIRECT_URI,
-  post_logout_redirect_uri: process.env.OIDC_LOGOUT_REDIRECT_URI,
+  authority: process.env.OIDC_AUTHORITY!,
+  client_id: process.env.OIDC_CLIENT_ID!,
+  redirect_uri: process.env.OIDC_REDIRECT_URI!,
+  post_logout_redirect_uri: process.env.OIDC_LOGOUT_REDIRECT_URI!,
   scope: 'openid profile email',
   automaticSilentRenew: true,
   // userStore: new WebStorageStateStore({ store }),
