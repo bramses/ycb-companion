0141931bcd4d9f52fc0331c48afaaa37643a2f4d
Bram Adams <3282661+bramses@users.noreply.github.com>
2025-02-14 17:32:47 -0500

    refactor oidc config to require env vars without defaults and add logging for OIDC_AUTHORITY value

--- src/libs/oidc.ts
+++ src/libs/oidc.ts
@@ -1,12 +1,12 @@
 import { UserManager } from 'oidc-client-ts';
 
+console.log('process.env.OIDC_AUTHORITY:', process.env.OIDC_AUTHORITY);
+
 const oidcConfig = {
-  authority: process.env.OIDC_AUTHORITY || 'http://localhost:8080/realms/ycb',
-  client_id: process.env.OIDC_CLIENT_ID || 'ycb',
-  redirect_uri:
-    process.env.OIDC_REDIRECT_URI || 'http://localhost:3000/signin-callback',
-  post_logout_redirect_uri:
-    process.env.OIDC_LOGOUT_REDIRECT_URI || `http://localhost:3000/`,
+  authority: process.env.OIDC_AUTHORITY,
+  client_id: process.env.OIDC_CLIENT_ID,
+  redirect_uri: process.env.OIDC_REDIRECT_URI,
+  post_logout_redirect_uri: process.env.OIDC_LOGOUT_REDIRECT_URI,
   scope: 'openid profile email',
   automaticSilentRenew: true,
   // userStore: new WebStorageStateStore({ store }),
