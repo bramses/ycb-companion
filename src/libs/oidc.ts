8720146a4deb65e90f571781f457a93bdf801fa5
Bram Adams <3282661+bramses@users.noreply.github.com>
2025-02-14 17:58:13 -0500

    Update oidc.ts to use hardcoded values for authority and redirect URIs, removing reliance on environment variables

--- src/libs/oidc.ts
+++ src/libs/oidc.ts
@@ -1,10 +1,25 @@
 import { UserManager } from 'oidc-client-ts';
 
+// OIDC_AUTHORITY=http://localhost:8080/realms/ycb
+// OIDC_CLIENT_ID=ycb
+// OIDC_REDIRECT_URI=http://localhost:3000/signin-callback
+// OIDC_LOGOUT_REDIRECT_URI=http://localhost:3000/
+
+// const oidcConfig = {
+//   authority: process.env.OIDC_AUTHORITY!,
+//   client_id: process.env.OIDC_CLIENT_ID!,
+//   redirect_uri: process.env.OIDC_REDIRECT_URI!,
+//   post_logout_redirect_uri: process.env.OIDC_LOGOUT_REDIRECT_URI!,
+//   scope: 'openid profile email',
+//   automaticSilentRenew: true,
+//   // userStore: new WebStorageStateStore({ store }),
+// };
+
 const oidcConfig = {
-  authority: process.env.OIDC_AUTHORITY!,
-  client_id: process.env.OIDC_CLIENT_ID!,
-  redirect_uri: process.env.OIDC_REDIRECT_URI!,
-  post_logout_redirect_uri: process.env.OIDC_LOGOUT_REDIRECT_URI!,
+  authority: 'http://keycloak:8080/realms/ycb',
+  client_id: 'ycb',
+  redirect_uri: 'https://yourcommonbase.com/signin-callback',
+  post_logout_redirect_uri: 'https://yourcommonbase.com/',
   scope: 'openid profile email',
   automaticSilentRenew: true,
   // userStore: new WebStorageStateStore({ store }),
