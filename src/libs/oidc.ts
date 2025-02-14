b099907dc811ba0e2c6b484589efd0e11730c721
Bram Adams <3282661+bramses@users.noreply.github.com>
2025-02-14 16:46:59 -0500

    feat: Add OIDC configuration to .env.sample and refactor oidc.ts to utilize environment variables for authentication settings

--- src/libs/oidc.ts
+++ src/libs/oidc.ts
@@ -1,10 +1,12 @@
 import { UserManager } from 'oidc-client-ts';
 
 const oidcConfig = {
-  authority: 'http://localhost:8080/realms/ycb',
-  client_id: 'ycb-public',
-  redirect_uri: `http://localhost:3000/signin-callback`,
-  post_logout_redirect_uri: `http://localhost:3000/`,
+  authority: process.env.OIDC_AUTHORITY || 'http://localhost:8080/realms/ycb',
+  client_id: process.env.OIDC_CLIENT_ID || 'ycb',
+  redirect_uri:
+    process.env.OIDC_REDIRECT_URI || 'http://localhost:3000/signin-callback',
+  post_logout_redirect_uri:
+    process.env.OIDC_LOGOUT_REDIRECT_URI || `http://localhost:3000/`,
   scope: 'openid profile email',
   automaticSilentRenew: true,
   // userStore: new WebStorageStateStore({ store }),
